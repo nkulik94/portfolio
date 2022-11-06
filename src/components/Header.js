ac2f15b08eb1e171f8e5c93dcc8de1fef9f4b741
Naftali Kulik <nkulik1994@gmail.com>
2022-11-06 10:20:01 -0500

    Moved bio info for header into separate component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,11 @@
 import React from "react";
 import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Avatar from '@mui/material/Avatar';
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import ShortBio from "./ShortBio";
 
 function Header() {
     return (
         <Container>
-            <Box sx={{width: '50%', textAlign: 'center', margin: 'auto', padding: '1%'}}>
-                <Avatar sx={{height: '150px', width: '150px', margin: 'auto'}} src='https://i.ibb.co/Fq8L1h4/Whats-App-Image-2022-11-06-at-8-04-05-AM.jpg' />
-                <Typography variant="h4">Naftali Kulik</Typography>
-                <Typography variant='h6'>Full Stack Software Engineer</Typography>
-                <Typography variant='title'>Skills: JavaScript | React | Ruby | Rails | SQL</Typography>
-                <br/>
-                <Typography variant="title">Currently Learning: Python, AWS</Typography>
-            </Box>
+            <ShortBio />
         </Container>
     )
 }
