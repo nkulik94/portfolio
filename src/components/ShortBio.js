0f81af4da25fe0d48d93e7851ad4b5a1a94bcaec
Naftali Kulik <nkulik1994@gmail.com>
2022-11-20 12:19:12 -0500

    Link profile pic

--- src/components/ShortBio.js
+++ src/components/ShortBio.js
@@ -2,11 +2,14 @@ import React from "react";
 import Avatar from '@mui/material/Avatar';
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 function ShortBio() {
     return (
         <Box sx={{width: '50%', textAlign: 'center', margin: 'auto', padding: '1%'}}>
-            <Avatar sx={{height: '150px', width: '150px', margin: 'auto'}} src='https://i.ibb.co/Fq8L1h4/Whats-App-Image-2022-11-06-at-8-04-05-AM.jpg' />
+            <Link href="https://www.linkedin.com/in/naftali-kulik-se/" target="_blank">
+                <Avatar sx={{height: '150px', width: '150px', margin: 'auto'}} src='https://i.ibb.co/Fq8L1h4/Whats-App-Image-2022-11-06-at-8-04-05-AM.jpg' />
+            </Link>
             <Typography variant="h4">Naftali Kulik</Typography>
             <Typography variant='h6'>Full Stack Software Engineer</Typography>
             <Typography variant='title'>Skills: JavaScript | React | Ruby | Rails | SQL</Typography>
