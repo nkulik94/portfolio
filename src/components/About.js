8511ef4e77b8d272199eaf77014f4538060726c0
Naftali Kulik <nkulik1994@gmail.com>
2022-11-21 10:21:29 -0500

    Add open graph description

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+
+function About() {
+    return (
+        <Container>
+            <Paper>
+                
+            </Paper>
+        </Container>
+    )
+}
+
+export default About
