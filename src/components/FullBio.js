596db75dc10b47d5cefd7dfdd661a9a88e7479d1
Naftali Kulik <nkulik1994@gmail.com>
2023-01-15 13:55:34 -0500

    Update bio

--- src/components/FullBio.js
+++ src/components/FullBio.js
@@ -9,7 +9,7 @@ function FullBio() {
         <Paper sx={{padding: '1rem'}}>
             <Typography variant='h4'>Bio:</Typography>
             <Typography variant="body" component='p'>
-                I am currently a Software developer for Senior Planning Services in Lakewood, NJ.
+                I am currently a Software developer for BLVD Software Solutions in Lakewood, NJ.
                 My job involves writing Python code when I can and manipulating QuickBase when I must.
             </Typography>
             <Typography variant="body" component='p'>
